dab0e681671b77a2f0ad104c316d0559259f15cb
Kamargul <tursynzanovakamargul@gmail.com>
2024-11-25 12:35:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let option = prompt("2 Сан енгізіңіз!");
+
+
+switch (option) {
+  case "24":
+    alert("Сіз 24 таңдадыңыз");
+    break;
+
+  case "3":
+    alert("Сіз 3 таңдадыңыз");
+    break;
+
+
+  default:
+    alert("Қате енгізу");
+
+
+    function calculator(, b) {
+      return a + b;
+
+    }
+
+    console.log(calculator(24, 3,));
+    console.log(calculator(24, 3,));
+    console.log(calculator(24, 3,));
+
+
+    function calculator(a, b,) {
+      switch (c) {
+        case "+":
+          return a + b;
+          break;
+        case "*":
+          return a * b;
+          break;
+
+      }
+
+    }
+
+}
